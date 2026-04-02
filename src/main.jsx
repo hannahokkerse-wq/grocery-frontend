dcad7695c63856b2a6513cd55e9df65fa211f3a2
hannahokkerse-wq <hannah.okkerse@gmail.com>
2026-04-02 22:00:26 +0200

    Update main.jsx
    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
-"isOffer": true,
-"discountPercent": 25,
-"dealLabel": "Bonus"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
